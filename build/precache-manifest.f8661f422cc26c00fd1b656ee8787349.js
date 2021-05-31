5bcc38ad2691c21a3041a3cf4ef0002409c2aaa9
chrisschnaars <schnaarschris@gmail.com>
2021-05-31 08:21:30 -0400

    Update build location and rebuild

---
+++ build/precache-manifest.f8661f422cc26c00fd1b656ee8787349.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aead8ca702d3f4a41fb20c5e3a3eeb65",
+    "url": "/bubble-tones/index.html"
+  },
+  {
+    "revision": "e863e8d585f06db816fc",
+    "url": "/bubble-tones/static/css/main.eeb28a4f.chunk.css"
+  },
+  {
+    "revision": "5fcfe4be5f8b1332ebd3",
+    "url": "/bubble-tones/static/js/2.74f36061.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/bubble-tones/static/js/2.74f36061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e863e8d585f06db816fc",
+    "url": "/bubble-tones/static/js/main.262a38ff.chunk.js"
+  },
+  {
+    "revision": "9e4936bd9ad04909fefb",
+    "url": "/bubble-tones/static/js/runtime-main.2c1210ea.js"
+  },
+  {
+    "revision": "5498aa6f36b3e251cd694608245dcdf0",
+    "url": "/bubble-tones/static/media/Close.5498aa6f.svg"
+  },
+  {
+    "revision": "a30df480d0f7512dbf6e51c260ba1212",
+    "url": "/bubble-tones/static/media/Info.a30df480.svg"
+  },
+  {
+    "revision": "ecd8e2c3ebcdcfc53316acd45444a988",
+    "url": "/bubble-tones/static/media/Pause.ecd8e2c3.svg"
+  },
+  {
+    "revision": "b9af76aa7f3133da7be7ebd73476f6c9",
+    "url": "/bubble-tones/static/media/Play.b9af76aa.svg"
+  }
+]);
